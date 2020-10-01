fca858e9d215b0152e109665af03b20a89808a96
Fällman <andmarku@student.chalmers.se>
2020-10-01 11:56:06 +0200

    improved data cleaning: fixed bug in data (duplicates) and reduced the size of the total date frame

--- stock_data/stock_data.py
+++ stock_data/stock_data.py
@@ -4,11 +4,11 @@ import numpy as np
 import pandas as pd
 from pandas.tseries.offsets import BDay # BDay for business day
 
-#todo temporary definition of constants
-def create_stock_df(nr_days_to_trade=100, start_date_str='2017-01-17', nr_days_of_history=10):
+def create_stock_df(nr_days_to_trade=500, start_date_str='2017-01-17', nr_days_of_history=10):
     """
     Wrapper for importing the stock data
     """
+
     return(import_stock_data(nr_days_of_history, nr_days_to_trade, start_date_str))
 
 
@@ -17,6 +17,7 @@ def calculateDates(nr_days_of_history, nr_days_to_trade, start_date_str):
     Calculates correct start and end dates for the data frame.
     - The start date is moved backward in order to include the history_window for the first day of trading.
     """
+
     # create a start date
     start_date = pd.Timestamp(start_date_str)
 
@@ -29,27 +30,15 @@ def calculateDates(nr_days_of_history, nr_days_to_trade, start_date_str):
     return {'start_date':start_date,'end_date':end_date}
 
 
-# todo do proper check for file not found
-# todo temporary limitation of the nr of stocks
-def import_stock_data(nr_days_of_history, nr_days_to_trade, start_date_str):
+def read_in_as_list_of_dfs(all_files,start_date,end_date):
     """
-    Imports the relevant data from the text file system as a pandas data frame
+    Create list of cleaned data frames of the stock data in the specified files
     """
-    # OBS!!!!!! temporary limitation for the size of the data frame
-    size_of_subset = 100
-    
-    # pick out the desired start and end dates
-    dates = calculateDates(nr_days_of_history = nr_days_of_history, nr_days_to_trade = nr_days_to_trade, start_date_str = start_date_str)
-
-    # list the files to be read
-    path = "./data/full_history/*.csv"
-    all_files = np.array(glob.glob(path))
-
     # create an empty list to add each (relevant) stock's data frame
     li=[]
 
     # go through all the files in the folder
-    for filename in all_files[:size_of_subset]:
+    for filename in all_files:
         # read in the file
         df = pd.read_csv(filename, parse_dates=[0])
         
@@ -59,19 +48,58 @@ def import_stock_data(nr_days_of_history, nr_days_to_trade, start_date_str):
         df = df.rename(columns={'open': stock_name})
         
         # fit to time window: selct only rows with relevant dates and ignore stocks without relevant values
-        df = df.loc[(df['date'] > dates['start_date']) & (df['date'] < dates['end_date'])]
+        df = df.loc[(df['date'] > start_date) & (df['date'] < end_date)]
         if df.empty:
             continue
         
-        # prepare for concatenation: prep to join on dates and add to list of dfs
+        # prepare to join on dates 
         df=df.set_index('date')
+
+        # remove duplicates
+        df = df.loc[np.logical_not(df.index.duplicated())]
+
+        # add to list of dfs
         li.append(df)
+
+    return(li)
+
+
+def clean_combined_df(df):
+    """
+    Reduce the size and time unnecessary info (date)
+    """
+    # remove dates (anonymises the time window somewhat)
+    df = df.reset_index(drop=True)
+
+    # set non-existing recordings to zero (stock had zero value at the time)
+    df = df.fillna(0)
+
+    # select the smallest suitable type of float for the data
+    df = df.astype(np.float16)
+
+    return(df)
+
+
+def import_stock_data(nr_days_of_history, nr_days_to_trade, start_date_str):
+    """
+    Imports the relevant data from the text file system as a pandas data frame
+    """
+    
+    # pick out the desired start and end dates
+    dates = calculateDates(nr_days_of_history = nr_days_of_history, nr_days_to_trade = nr_days_to_trade, start_date_str = start_date_str)
+
+    # list the files to be read
+    path = "./data/full_history/*.csv"
+    all_files = np.array(glob.glob(path))
+
+    # read in the data frames from the files
+    li = read_in_as_list_of_dfs(all_files, dates['start_date'], dates['end_date'])
 
     # combine into a single data frame
     df = pd.concat(li, join='outer', axis=1)
 
-    # remove dates (anonymises the time window somewhat)
-    #df.reset_index(drop=True)
+    # clean the combined data frame
+    df = clean_combined_df(df)
 
     return(df)
 
